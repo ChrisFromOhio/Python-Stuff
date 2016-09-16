758ea440bd0e5008c877d198df53b4d804b54a23
Chris <chrisdelauro11@gmail.com>
2016-09-16 11:54:47 -0400

    Delete racism.py

--- racism.py
+++
@@ -1,9 +0,0 @@
-def racism():
-    who = input("Who is in teamspeak right now? ")
-    who = who.lower()
-    if who in ['scuba']:
-        print('kill that nigger')
-    else:
-        print("its a good thing scuba isnt there")
-
-racism()
